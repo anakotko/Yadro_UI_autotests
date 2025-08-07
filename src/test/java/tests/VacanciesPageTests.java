3c108406c3e17bd0ab7718d0872a708d322b81a5
Anastasiia Kotko <mac@MacBook-Pro.local>
2025-08-07 13:59:48 +0700

    added new VacanciesPageTests

--- src/test/java/tests/VacanciesPageTests.java
+++ src/test/java/tests/VacanciesPageTests.java
@@ -1,13 +1,20 @@
 package tests;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.MainPage;
 import pages.VacanciesPage;
 
+@Epic("Страница 'Вакансии'")
+@Owner("anakotko")
+@Tag("yadro_test")
+@DisplayName("Проверка работы поиска на странице 'Вакансии'")
 public class VacanciesPageTests extends TestBase{
 
     MainPage mainPage = new MainPage();
@@ -18,7 +25,6 @@ public class VacanciesPageTests extends TestBase{
             "java", "qa", "c"
     })
     @Severity(SeverityLevel.CRITICAL)
-    @DisplayName("Открываем страницу Партнеры и видим заголовок 'Партнерская экосистема'")
     void vacanciesPageTest(String searchQuery) {
         mainPage.clickVacanciesPage();
         vacanciesPage.partnersPageSearch(searchQuery)
